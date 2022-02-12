d385c773413a52e7d6b6f11c93b12ace340d251e
Shaig-R <rustamovshaig5@gmail.com>
2022-02-12 11:03:32 +0100

    create Dashboard and Header components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
 import Login from "./component/Login";
 import ManualButton from './component/ManualButton';
-
+import Dashboard from "./component/Dashboard";
+import Header from "./component/Header";
+/*
+<Login />
+<div className='ManualButton'>
+<ManualButton/>
+</div>
+ */
 function App() {
 
     return (
             <div className='App'>
-                <Login />
-                <div className='ManualButton'>
-                <ManualButton/>
-                </div>
+                <Header />
+                <Dashboard />
             </div>
 
     )

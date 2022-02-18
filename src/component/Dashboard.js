9224381a0acfa17345a7a0d1cc36131101758de7
Shaig-R <rustamovshaig5@gmail.com>
2022-02-18 11:05:24 +0100

    dashboard & header

---
+++ src/component/Dashboard.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import './Dashboard.css';
+import Login from "./Login";
+
+function Dashboard() {
+
+    return(
+     <div className="Dashboard">
+         <h1>Dashboard</h1>
+     </div>
+
+    )
+
+}
+export default Dashboard;
